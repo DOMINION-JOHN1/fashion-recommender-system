ff974bcef5223440a43f4f5fb24e39e2170e405b
DOMINION JOHN <119686378+DOMINION-JOHN1@users.noreply.github.com>
2024-02-14 16:32:20 +0100

    upload

---
+++ run_file.py
@@ -0,0 +1,8 @@
+import pickle
+import os
+filenames = pickle.load(open('filenames.pkl','rb'))
+
+for file in os.listdir('images'):
+    filenames.append(os.path.join('images',file))
+
+pickle.dump(filenames,open('filenames.pkl','wb'))
